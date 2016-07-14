72f37f9f60aec68efe4b5b4d77fd0863a5c4e269
zeeshan <zeeshanansari38244@gmail.com>
2016-07-14 13:03:22 +0500

    Data type in php like Integer , Boolen , String while using if condition to check

---
+++ dataType.php
@@ -0,0 +1,21 @@
+<?php
+  
+
+  // String DataType
+  $name = "Xeeshan";
+  
+  // Integer DataType
+  $age = 18;
+ 
+  // boolen  DataType
+  $value1 = true;
+  $value2 = false;
+
+
+  if ($value1==false){
+     echo " Value is true";
+  } else{
+      echo " vaule is false";
+  }
+
+?>
