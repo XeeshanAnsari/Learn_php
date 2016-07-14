987e0e8b978333c6465ab5493b6af3a39476703c
zeeshan <zeeshanansari38244@gmail.com>
2016-07-14 12:45:13 +0500

    Array class is Completed !

---
+++ Array.php
@@ -0,0 +1,17 @@
+<?php
+  
+  $info = array ("Xeeshan","Zeeshanansari38244@gmail.com","18","Karachi","Pakistan","Male");
+
+
+  echo "<br> Your name : " . $info[0] . 
+   "<br> your email is : " . $info[1] .
+   "<br> your age is : " . $info[2] . 
+   "<br> your city is : " . $info[3] .
+   "<br> your Country is : " . $info[4] .
+   "<br> your Gender is :  " . $info[5] .
+   "<br><br><br> count of array ";
+
+   echo count ($info);
+  
+
+?>
