74ab0bd865abe23de557fca11e508dfac6212185
zeeshan <zeeshanansari38244@gmail.com>
2016-07-15 12:34:08 +0500

    if else with check the number is greater or not to ten

---
+++ do_while.php
@@ -0,0 +1,9 @@
+<?php
+   
+   $number = 0;
+   do{
+       $number++;
+      echo "<br>" . $number;
+   }while($number<=9)
+
+?>
---
+++ if_else.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>If Else</title>
+</head>
+<body>
+      
+    <form action="if_else.php" method="post" > 
+     
+       <lable>Enter a Number<input type="text" name ="number"  ></lable><br>
+       
+       <input type="submit" name="submit" value="Sign up">     
+     
+    </form>
+</body>
+</html>
+
+
+
+<?php
+   if(isset($_POST['submit'])){
+      if($_POST ['number'] <= 10 ){
+        echo "<br>NUmber is lesser to ten";
+      }else{
+        echo "<br>NUmber is greater to ten";
+      }
+  }
+
+?>
