15b6aaf711803608473384641af48083c2c78d43
zeeshan <zeeshanansari38244@gmail.com>
2016-07-15 11:28:04 +0500

    while loop programe 1 to 100 number is display

---
+++ commit.php
@@ -0,0 +1,9 @@
+<?php
+  
+// for single line commit
+  echo "HEre is Example of commit";
+
+/*     Multiple line commit 
+   you can used here paragraph       */
+
+?>
---
+++ while_loop.php
@@ -0,0 +1,8 @@
+<?php
+      
+  $number = 0;
+  while($number<= 99){
+      $number++;
+      echo "<br>" . $number;
+  }
+?>
