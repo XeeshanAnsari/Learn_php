17cc5644042ddbc90131e4a247f503868a817d7f
zeeshan <zeeshanansari38244@gmail.com>
2016-07-18 10:53:03 +0500

    Last Visited Date show with the help of COOKIES

--- cookies/lastVisitedCookie.php
+++ cookies/lastVisitedCookie.php
@@ -1,4 +1,16 @@
 <?php
 
+  $cookie_name="user_visit";
+  $month=time()+30*24*60*60;
+  setcookie('user_visit',date("m/d/y"),$month);
 
+?>
+
+
+<?php
+   if(isset($_COOKIE['user_visit'])){
+       $last_visit=$_COOKIE['user_visit'];
+       echo "Your last visited on this site on $last_visit";
+
+   }
 ?>
